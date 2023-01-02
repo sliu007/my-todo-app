d3dffb5f0424425f23fcde9943f4642121a53814
Sam <samuelliu_MD@yahoo.com>
2023-01-01 22:20:35 -0600

    fixing errors pt 3

--- functions.py
+++ functions.py
@@ -5,17 +5,17 @@ def get_todos(filepath=FILEPATH):
     """ Read a text file and return the list
     of to-do items.
     """
-    with open(filepath, 'r') as file:
-        todos = file.readlines()
-    return todos
+    with open(filepath, "r") as tl:
+        tl = tl.readlines()
+        return tl
 
 #print(help(get_todos()))
 
 def write_todos(todos_arg, filepath=FILEPATH):
     """ Write the to-do items list in the text file.
     """
-    with open('todos.txt', 'w') as file:
-        file.writelines(todos_arg)
+    with open(filepath, "w") as tl:
+        tl.writelines(todos_arg)
 
 #print(help(write_todos()))
 
